d726daa5a78e39648bc13884a022349b0c912181
qzmalekuz <qmalekuzzafar1235@gmail.com>
2026-02-19 18:25:08 +0530

    updated signup endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -10,7 +10,6 @@ const saltRounds = Number(process.env.SALT_ROUNDS) || 10;
 const router = Router();
 
 // Question - 1
-// signin endpoint
 
 router.post('/signup', async(req: Request, res: Response) => {
     try{
